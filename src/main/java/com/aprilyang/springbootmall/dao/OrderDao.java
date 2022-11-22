37b069e84eccfdc4980f40468e131eee74ce5e57
aprilyyw <116157797+aprilyyw@users.noreply.github.com>
2022-11-23 03:18:04 +0800

    訂單功能 - 查詢訂單列表

--- src/main/java/com/aprilyang/springbootmall/dao/OrderDao.java
+++ src/main/java/com/aprilyang/springbootmall/dao/OrderDao.java
@@ -1,11 +1,16 @@
 package com.aprilyang.springbootmall.dao;
 
+import com.aprilyang.springbootmall.dto.OrderQueryParams;
 import com.aprilyang.springbootmall.model.Order;
 import com.aprilyang.springbootmall.model.OrderItem;
 
 import java.util.List;
 
 public interface OrderDao {
+
+    Integer countOrder(OrderQueryParams orderQueryParams);
+
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
 
     Order getOrderById(Integer orderId);
 
--- src/main/java/com/aprilyang/springbootmall/service/OrderService.java
+++ src/main/java/com/aprilyang/springbootmall/service/OrderService.java
@@ -1,9 +1,16 @@
 package com.aprilyang.springbootmall.service;
 
 import com.aprilyang.springbootmall.dto.CreateOrderRequest;
+import com.aprilyang.springbootmall.dto.OrderQueryParams;
 import com.aprilyang.springbootmall.model.Order;
 
+import java.util.List;
+
 public interface OrderService {
+
+    Integer countOrder(OrderQueryParams orderQueryParams);
+
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
 
     Order getOrderById(Integer orderId);
 
